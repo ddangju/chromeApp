0461495113faf6a015b7234e86cf8b45592eae38
ddangju <duswn4338@gmail.com>
2021-10-14 13:21:19 +0900

    list에 추가 가능, 새로고침해도 list 랜더

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,7 @@
 const todoForm = document.getElementById("todoList");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo");
-const toDos = [];
+let toDos = [];
 
 // function paintTodo(todo) {
 //   const list = document.createElement("li");
@@ -14,7 +14,8 @@ const toDos = [];
 //   console.log(list);
 //   todoList.append(list);
 // }
-function saveTodo() {
+function saveTodo(save) {
+  console.log(save);
   localStorage.setItem("todo", JSON.stringify(toDos));
 }
 
@@ -43,8 +44,29 @@ function handleSubmit(e) {
   todoInput.value = "";
   toDos.push(newTodo);
   paintTodo(newTodo);
-  saveTodo();
+  saveTodo(newTodo);
   //비워지기전에 inputValue읙 값을 함수 인자로 넣어준다.
 }
 
 todoForm.addEventListener("submit", handleSubmit);
+
+const saved = localStorage.getItem("todo");
+
+// function hello(item) {
+//   console.log(item, "나함수");
+// }
+
+// console.log(saved, "1");
+if (saved !== null) {
+  const parseTodo = JSON.parse(saved);
+  console.log(parseTodo, "리스트");
+  toDos = parseTodo;
+  // parseTodo.forEach(hello);
+  // parseTodo.forEach((item) => console.log(typeof item, "나아이템"));
+  parseTodo.forEach(paintTodo);
+}
+
+//1. 로컬스토리지에 접근하여 key의 값을 get한다.
+//2. if문을 통하여 로컬스토리지에 key의 값이 존재하는지 확인을 하고
+/// 존재한다면 string화 되어있는 배열을 parse해서 배열화를 해준다.
+///3. array에있는 각각의 요소에 대해 function을 실행시킨다
